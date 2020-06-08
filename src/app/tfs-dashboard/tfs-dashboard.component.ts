1ed4f6cc5d7948b017ed50f7790096cbf491ae9c
alicewang3314 <alice.wang3314@gmail.com>
2020-06-08 15:05:19 -0400

    remove log

--- src/app/tfs-dashboard/tfs-dashboard.component.ts
+++ src/app/tfs-dashboard/tfs-dashboard.component.ts
@@ -90,7 +90,6 @@ export class TfsDashboardComponent implements OnInit, OnChanges {
       });
     }
     else {
-      console.log('all pending report')
       this.Tasks$.subscribe(user => {
         Object.keys(user).length === 0 ? (user = this.cacheService.data) : user;
         this.cacheService.getAllPendingReport(user).subscribe(resp => {
@@ -117,8 +116,6 @@ export class TfsDashboardComponent implements OnInit, OnChanges {
         }
       );
     } else {
-
-      console.log('pending', this.selectedTabIndex.value)
       this.settingService.getProjectsTeamsFromDb().subscribe(
         s => {
           this.userSettings = s
@@ -127,7 +124,7 @@ export class TfsDashboardComponent implements OnInit, OnChanges {
             this.cacheService.getAllPendingReport(this.userSettings.tfsProjTeams).subscribe(
               resp => {
                 this.allPendingReport = resp;
-                console.log('pending',resp)
+                // console.log('pending',resp)
               }
             );
           }
