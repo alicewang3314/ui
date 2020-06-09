eda7291053a36f472f2b95adb2750231253786e5
alicewang3314 <alice.wang3314@gmail.com>
2020-06-09 17:05:30 -0400

    update

--- src/app/bug-dashboard-module/bug-dashboard.module.ts
+++ src/app/bug-dashboard-module/bug-dashboard.module.ts
@@ -4,14 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatCardModule } from '@angular/material/card';
 import { MatSelectModule } from "@angular/material/select";
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { BugDashboardRoutingModule } from './bug-dashboard.routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatListModule } from '@angular/material/list';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 
+
+import { BugDashboardRoutingModule } from './bug-dashboard.routing.module';
 import { BugDashboardComponent } from './bug-dashboard/bug-dashbard.component';
 import { BugDetailsDialog } from './bug-detail-dialog/bug-detail-dialog.component';
 import { BugReportCardComponent } from './bug-report-card/bug-report-card.component';
@@ -30,7 +40,16 @@ import { CacheService } from 'src/app/services/cache.service';
     MatSelectModule,
     MatExpansionModule,
     MatCardModule,
+    MatDialogModule,
     NgxChartsModule,
+    MatGridListModule,
+    MatListModule,
+    MatProgressSpinnerModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatCheckboxModule,
+    MatIconModule,
     BugDashboardRoutingModule,
   ],
   exports: [
