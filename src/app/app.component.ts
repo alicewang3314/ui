b0eeaa007acb3631f70de3fbeb023427a9f69f04
alicewang3314 <alice.wang3314@gmail.com>
2020-06-10 15:19:00 -0400

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from "@angular/core";
 import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 // import { SESSION_STORAGE, WebStorageService } from "angular-webstorage-service";
 @Component({
   selector: "app-root",
@@ -9,4 +10,18 @@ import { FormControl } from '@angular/forms';
 export class AppComponent {
   title = "tfs-reports";
   mode = new FormControl('over');
+  pagesWithSetting = ['tfs-dashboard'];
+
+  constructor(private router: Router) { }
+
+  get hasSetting(): boolean {
+    let path =  this.router.url;
+    path = path.startsWith('/') ? path.substring(1) : path;
+    const pathArr = path.split('/');
+    return this.pagesWithSetting.indexOf(pathArr[0]) !== -1;
+  }
+
+  getSetting(): void {
+
+  }
 }
