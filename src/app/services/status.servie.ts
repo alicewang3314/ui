8114d45798c7d7d0af35c5e92a5629e02397a7aa
alicewang3314 <alice.wang3314@gmail.com>
2020-06-18 16:16:44 -0400

    state

--- src/app/services/status.servie.ts
+++ src/app/services/status.servie.ts
@@ -6,9 +6,16 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class StatusService {
   private _activeTabIndex: BehaviorSubject<number>;
+  appState = {
+    ['/tfs-dashboard']: { activeTabIndex: 0 },
+    ['/error-logs-dashboard']: {},
+    ['/bug-dashboard']: {},
+  };
 
   constructor() {
-    this._activeTabIndex = new BehaviorSubject(0);
+    this.recoverState();
+    
+    this._activeTabIndex = new BehaviorSubject(this.appState["/tfs-dashboard"].activeTabIndex);
   }
 
   getActiveTabIndex(): Observable<number> {
@@ -17,5 +24,21 @@ export class StatusService {
 
   set activeTabIndex(index: number) {
     this._activeTabIndex.next(index);
+    this.saveState();
+  }
+
+  saveState() {
+    this.appState = {
+      ['/tfs-dashboard']: { activeTabIndex: this._activeTabIndex.value },
+      ['/error-logs-dashboard']: {},
+      ['/bug-dashboard']: {},
+    };
+
+    localStorage.setItem('app-state', JSON.stringify(this.appState));
+  }
+
+  recoverState() {
+    const savedState = localStorage.getItem('app-state');
+    if (savedState) this.appState = JSON.parse(savedState);
   }
 }
